993b7912e02dbfd9f62d171a8b15718abb6a80b4
mrWinsont <aleksandrgrin263@gmail.com>
2024-09-14 15:50:43 +0200

    +lab2

--- lab2/task1.py
+++ lab2/task1.py
@@ -2,17 +2,16 @@ from PIL import Image
 from argparse import ArgumentParser
 
 
-if __name__ == "__main__":
-    with Image.open("images/image1.jpg") as img:
-        r, g, b = img.split()
+with Image.open("images/image1.jpg") as img:
+    r, g, b = img.split()
 
-        result_img_width = img.width * 4
-        result_img = Image.new('RGB', (result_img_width, img.width))
+    newWidth = img.width * 4
+    output = Image.new('RGB', (newWidth, img.width))
 
-        result_img.paste(img)
-        result_img.paste(r.convert('RGB'), (img.width, 0))
-        result_img.paste(g.convert('RGB'), (img.width * 2, 0))
-        result_img.paste(b.convert('RGB'), (img.width * 3, 0))
+    output.paste(img)
+    output.paste(r.convert('RGB'), (img.width, 0))
+    output.paste(g.convert('RGB'), (img.width * 2, 0))
+    output.paste(b.convert('RGB'), (img.width * 3, 0))
 
-        result_img.show()
-        result_img.save("images/output1.jpg", "jpeg")
+    output.show()
+    output.save("images/output1.jpg", "jpeg")
